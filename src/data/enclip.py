723f52a4f6fe853e1e6a75dabfa1d8a934f69fe5
nicolafan <48762613+nicolafan@users.noreply.github.com>
2023-04-12 18:22:51 +0200

    Update enclip script

--- src/data/enclip.py
+++ src/data/enclip.py
@@ -1,14 +1,21 @@
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
-from transformers import CLIPModel, CLIPProcessor
-import numpy as np
 import os
-from tqdm import tqdm
+from pathlib import Path
+
+import numpy as np
+from dotenv import find_dotenv, load_dotenv
 from PIL import Image
+from tqdm import tqdm
+from transformers import CLIPModel, CLIPProcessor
 
 
-def main(images_dir, processed_data_dir):
+def main(images_dir, output_dir):
+    """Compute CLIP embeddings for images in dir
+
+    Args:
+        images_dir (Path): directory containing images
+        output_dir (Path): directory where to store the embeddings
+    """
     logger = logging.getLogger(__name__)
     logger.info(f"creating CLIP embeddings for images in {images_dir}")
 
@@ -20,23 +27,26 @@ def main(images_dir, processed_data_dir):
     filenames = sorted(os.listdir(images_dir))
 
     for i in tqdm(range(0, len(filenames), 4)):
-        batch_filenames = filenames[i:min(i+4, len(filenames))]
+        batch_filenames = filenames[i : min(i + 4, len(filenames))]
         batch_images = []
         for filename in batch_filenames:
             image = Image.open(images_dir / filename)
             batch_images.append(image)
-        inputs = processor(text=None, images=batch_images, return_tensors="pt", padding=True)
+        inputs = processor(
+            text=None, images=batch_images, return_tensors="pt", padding=True
+        )
         images_features = model.get_image_features(inputs["pixel_values"])
-        embeddings = np.concatenate((embeddings, images_features.detach().numpy()), axis=0)
+        embeddings = np.concatenate(
+            (embeddings, images_features.detach().numpy()), axis=0
+        )
 
-    clip_embeddings_path = processed_data_dir / "clip_embeddings.npy"
+    clip_embeddings_path = output_dir / "clip_embeddings.npy"
     np.save(clip_embeddings_path, embeddings)
     logger.info(f"CLIP embeddings created at {clip_embeddings_path}")
 
 
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
