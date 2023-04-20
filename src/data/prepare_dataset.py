52968eeedd84e3e84844edb744bc9f471fb1699e
nicolafan <48762613+nicolafan@users.noreply.github.com>
2023-04-20 19:54:35 +0200

    Update prepare_dataset script

--- src/data/prepare_dataset.py
+++ src/data/prepare_dataset.py
@@ -12,42 +12,54 @@ def _encode_multiclass_feature(examples, feature, encoder):
 
 
 def _binarize_multilabel_feature(examples, feature, binarizer):
-    examples[feature] = binarizer.transform([[] if tags is None else tags.split(", ") for tags in examples[feature]])
+    examples[feature] = binarizer.transform(
+        [[] if labels is None else labels.split(", ") for labels in examples[feature]]
+    )
     return examples
 
 
 def _get_fitted_ordinal_encoder(dataset, feature):
-    oe = OrdinalEncoder(categories="auto", handle_unknown="use_encoded_value", unknown_value=-1)
-    training_values = dataset["train"][feature].reshape(-1, 1)
+    oe = OrdinalEncoder(
+        categories="auto", dtype=np.int64, handle_unknown="use_encoded_value", unknown_value=-1
+    )
+    categories = [category for category in dataset["train"][feature] if category is not None]
+    training_values = np.array(categories).reshape(-1, 1)
     return oe.fit(training_values)
 
 
 def _get_fitted_multilabel_binarizer(dataset, feature):
     mlb = MultiLabelBinarizer()
-    training_values = dataset["train"][feature]
+    training_values = [
+        [] if labels is None else labels.split(", ")
+        for labels in dataset["train"][feature]
+    ]
     return mlb.fit(training_values)
 
 
 def get_prepared_dataset_for_multiclassification(data_dir):
     dataset = load_dataset("imagefolder", data_dir=data_dir)
 
-    multiclass_features = ("artist", "genre", "style")
-    multilabel_features = ("tags", "media")
+    multiclass_features = ["artist", "genre", "style"]
+    multilabel_features = ["tags", "media"]
 
-    ordinal_encoders = dict((feature, _get_fitted_ordinal_encoder(dataset, feature)) for feature in multiclass_features)
-    multilabel_binarizers = dict((feature, _get_fitted_multilabel_binarizer(dataset, feature)) for feature in multilabel_features)
+    ordinal_encoders = dict(
+        (feature, _get_fitted_ordinal_encoder(dataset, feature))
+        for feature in multiclass_features
+    )
+    multilabel_binarizers = dict(
+        (feature, _get_fitted_multilabel_binarizer(dataset, feature))
+        for feature in multilabel_features
+    )
 
-    for feature, encoder in ordinal_encoders:
-        dataset = dataset.map(partial(_encode_multiclass_feature, feature=feature, encoder=encoder))
+    for feature, encoder in ordinal_encoders.items():
+        dataset = dataset.map(
+            partial(_encode_multiclass_feature, feature=feature, encoder=encoder), batched=True
+        )
 
-    for feature, binarizer in multilabel_binarizers:
-        dataset = dataset.map(partial(_binarize_multilabel_feature, feature=feature, binarizer=binarizer))
+    for feature, binarizer in multilabel_binarizers.items():
+        dataset = dataset.map(
+            partial(_binarize_multilabel_feature, feature=feature, binarizer=binarizer), batched=True
+        )
 
     dataset = dataset.remove_columns(["caption", "human"])
-    return dataset
-
-
-
-
-
-
+    return dataset, ordinal_encoders, multilabel_binarizers
