e8e2542f9d58d1f2bac6523bd78be696eada7c56
nicolafan <48762613+nicolafan@users.noreply.github.com>
2023-04-20 19:52:20 +0200

    Load dirutils to access dirs

---
+++ src/utils/dirutils.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+def get_project_dir():
+    """Returns the path to the project directory."""
+    # return parent of parent of current file as pathlib Path
+    return Path(__file__).parent.parent
+
+
+def get_data_dir():
+    """Returns the path to the data directory."""
+    return get_project_dir() / "data"
+
+
+def get_models_dir():
+    """Returns the path to the models directory."""
+    return get_project_dir() / "models"
