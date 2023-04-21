dc34b13f08f0409affc17a4118bf3ce0d2286191
nicolafan <48762613+nicolafan@users.noreply.github.com>
2023-04-21 14:20:37 +0200

    Document dirutils

--- src/utils/dirutils.py
+++ src/utils/dirutils.py
@@ -2,16 +2,27 @@ from pathlib import Path
 
 
 def get_project_dir():
-    """Returns the path to the project directory."""
-    # return parent of parent of current file as pathlib Path
+    """Returns the path to the project directory.
+
+    Returns:
+        project_dir (pathlib.Path): path to the project directory
+    """
     return Path(__file__).parent.parent.parent
 
 
 def get_data_dir():
-    """Returns the path to the data directory."""
+    """Returns the path to the data directory.
+
+    Returns:
+        data_dir (pathlib.Path): path to the data directory
+    """
     return get_project_dir() / "data"
 
 
 def get_models_dir():
-    """Returns the path to the models directory."""
+    """Returns the path to the models directory.
+
+    Returns:
+        models_dir (pathlib.Path): path to the models directory
+    """
     return get_project_dir() / "models"
